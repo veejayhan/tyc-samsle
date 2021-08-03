3ad3c346ed82643a07837a086f65cfcbc8603bab
veejayhan <veejayhan@163.com>
2021-08-03 17:03:04 +0800

    提交branch1

---
+++ src/main/java/com/example/demo/samsle/HelloWorld2.java
@@ -0,0 +1,11 @@
+package com.example.demo.samsle;
+
+/**
+ * @author 韩维杰
+ * @date 2021/8/3 4:56 下午
+ */
+public class HelloWorld2 {
+    public static void main(String[] args) {
+        System.out.println("Hello world2");
+    }
+}
