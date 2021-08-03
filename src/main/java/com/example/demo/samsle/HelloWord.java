c1bee6ca27fead0dd07ea6942e6f0dfedbd8ee94
veejayhan <veejayhan@163.com>
2021-08-03 10:23:15 +0800

    hell oworld

---
+++ src/main/java/com/example/demo/samsle/HelloWord.java
@@ -0,0 +1,11 @@
+package com.example.demo.samsle;
+
+/**
+ * @author 韩维杰
+ * @date 2021/8/3 10:20 上午
+ */
+public class HelloWord {
+    public static void main(String[] args) {
+        System.out.println("Hello word");
+    }
+}
