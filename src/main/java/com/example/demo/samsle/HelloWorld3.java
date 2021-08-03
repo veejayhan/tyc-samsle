3516669db7f6ee4b4a532db7fd92a262509cbf7e
veejayhan <veejayhan@163.com>
2021-08-03 13:38:25 +0800

    modified helloworld3

--- src/main/java/com/example/demo/samsle/HelloWorld3.java
+++ src/main/java/com/example/demo/samsle/HelloWorld3.java
@@ -6,6 +6,6 @@ package com.example.demo.samsle;
  */
 public class HelloWorld3 {
     public static void main(String[] args) {
-        System.out.println("Hello world 3");
+        System.out.println("Hello world 3, modified!");
     }
 }
