4b8ac9063d3d8854b51fdcd658d5abfb4427e22c
veejayhan <veejayhan@163.com>
2021-08-11 15:36:55 +0800

    update master branch

--- src/main/java/com/example/demo/samsle/HelloWord1.java
+++ src/main/java/com/example/demo/samsle/HelloWord1.java
@@ -4,8 +4,8 @@ package com.example.demo.samsle;
  * @author 韩维杰
  * @date 2021/8/3 10:20 上午
  */
-public class HelloWord {
+public class HelloWord1 {
     public static void main(String[] args) {
-        System.out.println("Hello word");
+        System.out.println("Hello word 1");
     }
 }
--- src/main/java/com/example/demo/samsle/HelloWorld2.java
+++ src/main/java/com/example/demo/samsle/HelloWorld2.java
@@ -2,10 +2,10 @@ package com.example.demo.samsle;
 
 /**
  * @author 韩维杰
- * @date 2021/8/3 10:55 上午
+ * @date 2021/8/11 3:35 下午
  */
-public class HelloWorld3 {
+public class HelloWorld2 {
     public static void main(String[] args) {
-        System.out.println("Hello world 3");
+        System.out.println("Hello world 2");
     }
 }
