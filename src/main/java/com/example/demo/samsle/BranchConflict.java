a48c56f1e2a6354e36a8d9ecddc4431647278f51
veejayhan <veejayhan@163.com>
2021-08-11 16:44:13 +0800

    branch-0-0 commit, BranchConflict

---
+++ src/main/java/com/example/demo/samsle/BranchConflict.java
@@ -0,0 +1,11 @@
+package com.example.demo.samsle;
+
+/**
+ * @author 韩维杰
+ * @date 2021/8/11 4:42 下午
+ */
+public class BranchConflict {
+    public static void main(String[] args) {
+        System.out.println("branch-0-0 commit, BranchConflict");
+    }
+}
